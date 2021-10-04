c5214a684c73d4cbdbf4f6dce806bd854408d036
Adi Putra <73918462+Putra-0@users.noreply.github.com>
2021-10-04 11:43:57 +0700

    Add files via upload

---
+++ libernet/footer.php
@@ -0,0 +1,5 @@
+<footer style="color:#2CC300;" class="text-center">
+    <div class="nganu slide">© Design by&nbsp;<a href="https://github.com/Putra-0">ADI-PUTRA</a></div>
+    © 2021 <a href="https://github.com/lutfailham96/libernet">Libernet</a> v1.5.3. All rights reserved.
+
+</footer>
