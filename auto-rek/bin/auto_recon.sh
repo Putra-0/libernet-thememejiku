c54d40fb368c0ef88c131b77feb6df9c7208e468
Adi Putra <73918462+Putra-0@users.noreply.github.com>
2021-10-30 15:35:55 +0700

    Add files via upload

--- auto-rek/bin/auto_recon.sh
+++ auto-rek/bin/auto_recon.sh
@@ -16,32 +16,25 @@ TUNNEL_MODE="$(grep 'mode":' ${SYSTEM_CONFIG} | awk '{print $2}' | sed 's/,//g; 
 function loop() {
 n=0
 while [ 1 ]; do
-  #ping with timeout 10 seconds
-  ping -c 1 -W 10 -w 10 bing.com
-  ret=$?
-  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: Green\">[AUTO-RC]ping result $ret</span>"
-#echo ping result $ret
-  if [ $ret -eq 0 ]; then
-  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: Green\">[AUTO-RC]ping ok</span>"
-    #echo ping ok
-    sleep 30
+  wan=$(curl --connect-timeout 30 'https://api.ipify.org/?format=json' | jq '.ip' | sed ' s/"//g')
+  ip=$(jq .server '/root/libernet/system/config.json' | sed ' s/"//g')
+echo $wan $ip
+  if  [ $wan = $ip ]; then
+  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: Green\">Checking connection = OK! ${ip} </span>"
+    echo wan cocok
+	#hg680p.sh -lan on &>/dev/null
+    sleep 60
 	n=0
-	#ipCheck
   else 
+  #hg680p.sh -lan off &>/dev/null
     echo ping fail
-    "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: red\">[AUTO-RC]fail ${n}</span>"
     n=$((n+1))
-    # when wan-dhcp fail, 
-    # net is unreachable and ping return without any delay
-    # using sleep 1 avoid fail count overflow too fast
-    sleep 1
+	"${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: red\">Connection Fail! ${n}</span>"
+    sleep 2
   fi
-
   echo fail counter $n
   if [ $n -gt 5 ]; then
-    # in case of wan-dhcp fail total time to reboot is 1 min (60 seconds)
-    # in case of ping-timeout total time to reboot is 11 min (660 seconds)
-    "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: green\">[AUTO-RC]re-connecting</span>"
+	"${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: green\">Re-Connecting</span>"
     n=0
     recon
   fi
@@ -58,37 +51,37 @@ recon(){
 
 function start_services() {
   # write to service log
-  #"${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Tunnel Service"
+  #"${LIBERNET_DIR}/bin/log.sh" -w "Re-Start Tunnel Service"
   case "${TUNNEL_MODE}" in
     "0")
-	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start SSH Service"
+	  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start SSH Service"
       "${LIBERNET_DIR}/bin/ssh.sh" -r
       ;;
     "1")
-	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start v2ray Service"
+	  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start v2ray Service"
       "${LIBERNET_DIR}/bin/v2ray.sh" -r
       ;;
     "2")
-	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start SSH-SSL Service"
+	  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start SSH-SSL Service"
       "${LIBERNET_DIR}/bin/ssh-ssl.sh" -r
       ;;
     "3")
-	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Trojan Service"
+	  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start Trojan Service"
       "${LIBERNET_DIR}/bin/trojan.sh" -r
       ;;
     "4")
-	  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start shadowsocks Service"
+	  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start shadowsocks Service"
       "${LIBERNET_DIR}/bin/shadowsocks.sh" -r
       ;;
   esac
-  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Re-Start Tun2Socks"
+  "${LIBERNET_DIR}/bin/log.sh" -w "Re-Start Tun2Socks"
   "${LIBERNET_DIR}/bin/tun2socks.sh" -v
   sleep 5
-  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: blue\">[AUTO-RC]Checking...</span>"
+  "${LIBERNET_DIR}/bin/log.sh" -w "<span style=\"color: blue\">Checking...</span>"
 }
 
 function stop_services() {
-  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Stopping Tunnel Service"
+  "${LIBERNET_DIR}/bin/log.sh" -w "Stopping Tunnel Service"
   case "${TUNNEL_MODE}" in
     "0")
       "${LIBERNET_DIR}/bin/ssh.sh" -s
@@ -109,7 +102,7 @@ function stop_services() {
       "${LIBERNET_DIR}/bin/openvpn.sh" -s
       ;;
   esac
-  "${LIBERNET_DIR}/bin/log.sh" -w "[AUTO-RC]Stopping Tun2Socks"
+  "${LIBERNET_DIR}/bin/log.sh" -w "Stopping Tun2Socks"
   "${LIBERNET_DIR}/bin/tun2socks.sh" -w
 }
 
